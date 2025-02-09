29e0762baa825abf15c98897db3ac403be02a6c2
MouhanadAhmed <houdacris88@gmail.com>
2025-02-10 01:34:35 +0200

    signin

--- src/components/Authpage/LoginCard.tsx
+++ src/components/Authpage/LoginCard.tsx
@@ -4,10 +4,8 @@ import Image from "next/image";
 import Link from "next/link";
 import {
   TextInput,
-  Checkbox,
   PasswordInput,
   Group,
-  Button,
 } from "@mantine/core";
 import { IconMailFast } from "@tabler/icons-react";
 import { useForm } from "@mantine/form";
@@ -15,36 +13,68 @@ import { showNotification } from "@mantine/notifications";
 import AuthPageConfig from "src/constants/ConfigAuthPage";
 import PrimaryButton from "../Buttons/PrimaryButton";
 import { ConfigBasicInfo } from "src/constants/ConfigBasics";
-
+import { authClient } from '@/lib/auth/client';
+import { useUser } from '@/hooks/use-user';
+import { useRouter } from "next/navigation";
 // authentication page
 // login page
 
+
 const LoginForm = () => {
-  // Submit function function
-  function submit(value: any) {
-    // Toast notification
-    showNotification({
-      title: "Success",
-      message: "Login as " + value.email.toString(),
-      color: "green",
-    });
-  }
+  const router = useRouter();
+
+  const { checkSession } = useUser();
+
+
+  const [isPending, setIsPending] = React.useState<boolean>(false);
+
+    const submit = React.useCallback(
+      async (values:any): Promise<void> => {
+        setIsPending(true);
+        const { error } = await authClient.signInWithPassword(values);
+  
+        if (error) {
+          showNotification({
+            title: "Error",
+            message: error.toString(),
+            color: "red",
+          });
+          setIsPending(false);
+          return;
+        }
+        setIsPending(false);
+              showNotification({
+        title: "Success",
+        message: "Login as " + values.email.toString(),
+        color: "green",
+      });
+        // Refresh the auth state
+        await checkSession?.();
+  
+        // UserProvider, for this case, will not refresh the router
+        // After refresh, GuestGuard will handle the redirect
+        router.replace('/')
+        router.refresh();
+      },
+      [checkSession, router]
+    );
 
   // Sign in form validations and initialization
   const form = useForm({
     initialValues: {
       email: "",
       password: "",
-      termsOfService: false,
     },
 
     // Email validation
     validate: {
       email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+      password: (value) => (/^\S/.test(value) ? null : "Invalid password"),
     },
   });
 
   return (
+
     <div className="rounded-default flex h-full w-full flex-col-reverse space-x-2 bg-white shadow-lg dark:bg-slate-900 dark:text-white/90 laptop:flex-row">
       {/* left */}
       <div className="basis-1/2">
@@ -77,6 +107,7 @@ const LoginForm = () => {
                 label="Email"
                 placeholder="your@email.com"
                 {...form.getInputProps("email")}
+                onBlur={() => form.validateField("email")}
                 classNames={{
                   label: "dark:text-white",
                   input: "dark:bg-slate-800 dark:text-white/90",
@@ -88,21 +119,22 @@ const LoginForm = () => {
                 placeholder="Password"
                 label="Password"
                 {...form.getInputProps("password")}
+                onBlur={() => form.validateField("password")}
+                
                 classNames={{
                   label: "dark:text-white",
                   input: "dark:bg-slate-800 dark:text-white/90",
                 }}
               />
 
-              <Checkbox
-                mt="md"
-                label="I agree to term and conditions"
-                {...form.getInputProps("termsOfService", { type: "checkbox" })}
-              />
-
               {/* Submit button */}
               <Group mt="md">
-                <PrimaryButton text="Submit" icon={<IconMailFast />} />
+                <PrimaryButton
+                  text="Submit"
+                  disabled={!(form.errors && Object.keys(form.errors).length === 0 && form.errors.constructor === Object) && isPending}
+                  onClick={(values) => submit(form.values)}
+                  icon={<IconMailFast />}
+                />
               </Group>
             </form>
             {/* Register link */}
@@ -121,6 +153,6 @@ const LoginForm = () => {
       </div>
     </div>
   );
-};
+}
 
 export default LoginForm;
---
+++ src/lib/auth/client.ts
@@ -0,0 +1,154 @@
+'use client';
+
+import axiosInstance from '@/services/axios.instance';
+import axios, { type AxiosError } from 'axios';
+
+import type { LoggedInUser, User } from '@/types/user';
+
+import { API_URL } from '../env-config';
+
+const user = {
+  name: 'Sofia Rivers',
+  _id: 'USR-000',
+  avatar: '/assets/avatar.png',
+  firstName: 'Sofia',
+  lastName: 'Rivers',
+  email: 'sofia@devias.io',
+  role: 'user',
+  lead_campaign: [1],
+} satisfies User;
+
+export interface SignUpParams {
+  name: string;
+  email: string;
+  password: string;
+  role?: string;
+  lead_campaign?: number;
+  recaptchaToken?: string;
+}
+
+export interface SignInWithOAuthParams {
+  provider: 'google' | 'discord';
+}
+
+export interface SignInWithPasswordParams {
+  email: string;
+  password: string;
+}
+
+export interface ResetPasswordParams {
+  email: string;
+}
+
+interface ErrorResponseData {
+  message?: string;
+}
+
+interface APIResponse {
+  data?: {
+    loggedInUser?: LoggedInUser;
+    message?: string;
+    jwt?: string;
+  };
+  error?: string;
+}
+
+// const router =useRouter();
+class AuthClient {
+  async signUp(params: SignUpParams): Promise<{ error?: string }> {
+    let response: APIResponse;
+
+    try {
+      response = await axios.post(`${API_URL}auth/Signup`, params);
+      return response;
+    } catch (error) {
+      const axiosError = error as AxiosError<ErrorResponseData>;
+      response = { error: axiosError.response?.data?.message };
+    }
+    // router.push(`${FRONT_URL}/auth/sign-in`);
+    //window.location.href = `${FRONT_URL}/auth/sign-in`;
+    // router.push(`${FRONT_URL}/auth/sign-in`)
+    // Refresh the auth state
+    // UserProvider, for this case, will not refresh the router
+    // After refresh, GuestGuard will handle the redirect
+    // router.refresh();
+    return { error: response.data?.message || response.error || ' ' };
+  }
+
+  async signInWithOAuth(_: SignInWithOAuthParams): Promise<{ error?: string }> {
+    return { error: 'Social authentication not implemented' };
+  }
+
+  async signInWithPassword(params: SignInWithPasswordParams): Promise<{ error?: string }> {
+    let response: APIResponse;
+
+    try {
+      response = await axios.post(`${API_URL}auth/login`, params);
+    } catch (error) {
+      const axiosError = error as AxiosError<ErrorResponseData>;
+      response = { error: axiosError.response?.data?.message };
+    }
+
+    if (response?.data?.message === 'success') {
+      localStorage.setItem('custom-auth-token', String(response?.data?.jwt));
+
+      const userData = response?.data?.loggedInUser;
+      localStorage.setItem('custom-auth-role', String(userData?.role));
+      localStorage.setItem('custom-auth-timezone', String(userData?.timeZone ?? 'America/New_York'));
+      localStorage.setItem(
+        'custom-auth-user',
+        JSON.stringify({
+          name: userData?.name,
+          email: userData?.email,
+          role: userData?.role,
+        })
+      );
+
+      return {};
+    }
+
+    return { error: (response.data?.message ?? response.error) || '' };
+  }
+
+  async resetPassword(_: ResetPasswordParams): Promise<{ error?: string }> {
+    return { error: 'Password reset not implemented' };
+  }
+
+  async updatePassword(_: ResetPasswordParams): Promise<{ error?: string }> {
+    return { error: 'Update reset not implemented' };
+  }
+
+  async getUser(): Promise<{ data?: object | null; error?: string }> {
+    const token = localStorage.getItem('custom-auth-token');
+
+    if (!token) {
+      return { data: null };
+    }
+
+    return { data: user };
+  }
+
+  async signOut(): Promise<{ error?: string }> {
+    let response: APIResponse;
+
+    try {
+      response = await axiosInstance.post(`${API_URL}auth/logout`, {
+        token: localStorage.getItem('custom-auth-token'),
+      });
+    } catch (error) {
+      const axiosError = error as AxiosError<ErrorResponseData>;
+      response = { error: axiosError.response?.data?.message };
+    }
+
+    localStorage.removeItem('custom-auth-token');
+    if (response?.data?.message === 'success') {
+      localStorage.removeItem('custom-auth-token');
+      localStorage.removeItem('custom-auth-role');
+      localStorage.removeItem('custom-auth-user');
+      return {};
+    }
+    return { error: response?.data?.message ?? response.error ?? '' };
+  }
+}
+
+export const authClient = new AuthClient();

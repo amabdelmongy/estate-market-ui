a4a28adaf0cbd760e857e1994d33f1d4b973f6bc
Ahmed Mongy <amabdelmongy@gmail.com>
2025-02-09 12:35:25 +0100

    fix PrimaryButton error

--- src/components/Buttons/PrimaryButton.tsx
+++ src/components/Buttons/PrimaryButton.tsx
@@ -1,3 +1,4 @@
+import React, { forwardRef } from "react";
 import { Button } from "@mantine/core";
 import { ConfigColors } from "src/constants/ConfigColors";
 
@@ -5,28 +6,30 @@ interface IPrimaryButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   text?: string;
   icon?: React.ReactNode;
   fullWidth?: boolean;
-  color?: "danger" | "secondary" | "transparent" | undefined;
+  color?: "danger" | "secondary" | "transparent";
 }
 
-const PrimaryButton = ({
-  text,
-  icon,
-  fullWidth,
-  color,
-  ...rest
-}: IPrimaryButton) => {
-  return (
-    <Button
-      leftSection={icon}
-      {...rest}
-      fullWidth={fullWidth}
-      style={{
-        backgroundColor: color ? ConfigColors.danger : ConfigColors.primary,
-      }}
-    >
-      {text}
-    </Button>
-  );
-};
+const PrimaryButton = forwardRef<HTMLButtonElement, IPrimaryButton>(
+  ({ text, icon, fullWidth, color, ...rest }, ref) => {
+    const backgroundColor =
+      color && color in ConfigColors
+        ? ConfigColors[color as keyof typeof ConfigColors]
+        : ConfigColors.primary;
+
+    return (
+      <Button
+        ref={ref}
+        leftSection={icon}
+        fullWidth={fullWidth}
+        style={{ backgroundColor }}
+        {...rest}
+      >
+        {text}
+      </Button>
+    );
+  },
+);
+
+PrimaryButton.displayName = "PrimaryButton";
 
 export default PrimaryButton;

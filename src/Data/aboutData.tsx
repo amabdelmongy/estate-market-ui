d3ea6e13295b8d53b9c68b68501a8d5016e7d8e9
medhatoraby88 <93214588+medhatoraby88@users.noreply.github.com>
2025-03-02 00:24:43 +0200

    update about (#14)

--- src/Data/aboutData.tsx
+++ src/Data/aboutData.tsx
@@ -2,39 +2,5 @@ interface IAboutPageData {
   story: string;
 }
 export const AboutPageData: IAboutPageData = {
-  story: ` Welcome to Real Estate Market, where dreams find their address and reality
-merges seamlessly with aspiration. Our journey began with a vision to
-redefine the real estate landscape, transcending the mundane and
-crafting living spaces that resonate with celestial elegance.
-Established in 2005, Real Estate Market has soared to the zenith of the
-industry, guided by a celestial compass that directs us towards
-innovation, excellence, and a touch of ethereal enchantment.
-Envisioned by our visionary founder, Archangel Realty, Real Estate Market
-took its first flight with a celestial blueprint that aimed to create
-heavenly abodes for those seeking a touch of divine living. Our
-architects, inspired by the heavenly realms, meticulously sculpt each
-structure, ensuring that every corner emanates a sense of serenity and
-luxury. Our commitment to architectural brilliance is only matched by
-our dedication to providing a celestial experience for each of our
-cherished clients. In our celestial journey, we&apos;ve spread our wings
-across the realms of residential, commercial, and mixed-use
-developments. From the sprawling Gardens of Eden Residences to the
-towering Nebula Towers, each Real Estate Market property is a testament to
-our commitment to crafting living spaces that transcend the ordinary
-and embody the extraordinary. Our properties are not mere structures;
-they are portals to a heavenly lifestyle. At Real Estate Market, we
-understand that a home is not just a dwelling but a sanctuary, a haven
-where life&apos;s celestial moments unfold. That&apos;s why we go above and
-beyond to infuse each project with a touch of divine inspiration,
-creating spaces that resonate with joy, comfort, and sophistication.
-Our team of celestial experts, from celestial architects to celestial
-interior designers, collaborates harmoniously to bring forth the
-ethereal essence that defines Real Estate Market. As we continue to ascend
-to greater heights, we invite you to join us on this celestial
-journey. Whether you are seeking a heavenly residence, an otherworldly
-office space, or a celestial investment opportunity, Real Estate Market is
-your celestial companion in the pursuit of a life less ordinary.
-Welcome to the celestial realm of Real Estate Market, where every property
-is a piece of paradise, and every client is a cherished celestial
-being in our cosmic community.`,
+  story: ` Wtest11111.`,
 };
